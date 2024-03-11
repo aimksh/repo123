9865b7f0a9300693018bdbcb607a75b19a9c3197
soohwa.kim <aimksh@naver.com>
2024-03-11 16:41:00 +0900

    master commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] agrs){
         System.out.println("test");
         System.out.println("test");
-        System.out.println("test");
 
     }
 }
